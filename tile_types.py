8262b166fd5d5a964cd8b49469b0f1211da29626
KoteAnimen <sverh.il@yandex.ru>
2023-11-14 15:31:44 +0300

    add tile_types, but now ended

---
+++ tile_types.py
@@ -0,0 +1,22 @@
+from typing import Tuple
+
+import numpy as np
+
+# Tile graphics structured type compatible with Console.tiles_rgb.
+graphic_dt = np.dtype(
+    [
+        ("ch", np.int32),  # Unicode codepoint.
+        ("fg", "3B"),  # 3 unsigned bytes, for RGB colors. foreground
+        ("bg", "3B"), #background
+    ]
+)
+
+# Tile struct used for statically defined tile data.
+tile_dt = np.dtype(
+    [
+        ("walkable", np.bool),  # True if this tile can be walked over.
+        ("transparent", np.bool),  # True if this tile doesn't block FOV.
+        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+    ]
+)
+
